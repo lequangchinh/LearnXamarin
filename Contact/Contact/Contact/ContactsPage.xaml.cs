07381c5bdf11985be2d914c8eaef4481c138f840
Chinh LeQuang <lequangchinh791989@gmail.com>
2019-04-01 17:00:39 +0700

    re-upload demo controls view

---
+++ Contact/Contact/Contact/ContactsPage.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Contact
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class ContactsPage : ContentPage
+	{
+		public ContactsPage ()
+		{
+			InitializeComponent ();
+		}
+        void OnAddContactButtonClicked(object sender, EventArgs e)
+        {
+
+        }
+
+    }
+}
