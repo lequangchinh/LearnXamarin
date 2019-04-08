9c49a7985144b6aa0900e09e7baaed32617142c5
Chinh LeQuang <lequangchinh791989@gmail.com>
2019-04-08 09:04:28 +0700

    navigation and style

---
+++ DemoStyle/DemoStyle/DemoStyle/DemoDynamicStyle.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace DemoStyle
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class DemoDynamicStyle : ContentPage
+	{
+        bool originalStyle = true;
+		public DemoDynamicStyle ()
+		{
+			InitializeComponent ();
+            Resources["stySearchBar"] = Resources["styBlueSearchBar"];
+		}
+        void OnChangeStyleButtonClicked(object sender, EventArgs e)
+        {
+            if (originalStyle)
+            {
+                Resources["stySearchBar"] = Resources["styGreenSearchBar"];
+                originalStyle = false;
+            }
+            else
+            {
+                Resources["stySearchBar"] = Resources["styBlueSearchBar"];
+                originalStyle = true;
+            }
+        }
+    }
+}
